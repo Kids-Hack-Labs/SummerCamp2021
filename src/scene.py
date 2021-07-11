81572f5e7641eb97813d67a10b6efa10c27e2cda
Hercules Dias Campos <64754149+HerculesKHL@users.noreply.github.com>
2021-07-11 12:31:42 -0400

    Add engine

---
+++ src/scene.py
@@ -0,0 +1,15 @@
+#imports go here
+
+class Scene():
+    def __init__(self):
+        pass
+
+    def start(self):
+        #For debug purposes: if False, game doesn't load
+        return True
+    
+    def update(self, delta):
+        pass
+
+    def render(self, target):
+        pass
